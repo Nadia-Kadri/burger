ee92107116dc90d04b64f52f588f9637d3e289db
Nadia-Kadri <nadiagrace96@gmail.com>
2020-01-03 12:37:56 -0500

    folder structure and database connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const mysql = require("mysql");
+const keys = require("./keys.js");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: keys.password,
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Glazed Donut Burger", FALSE),
+  ("Veggie Burger", FALSE),
+  ("Pulled Pork Burger", FALSE),
+  ("Hawaiian Burger", FALSE),
+  ("American Burger", FALSE)
