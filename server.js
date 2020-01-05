2f63785f3f76eddbcd8eea5959c23e92f023f2f9
Nadia-Kadri <nadiagrace96@gmail.com>
2020-01-04 21:46:21 -0500

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
-const app = express();
-const exphbs = require("express-handlebars");
-const routes = require("./controllers/burgers_controller.js");
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
